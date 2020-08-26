73cdc37b90797dd1f4efa9136a97d07ebc6e0d27
jillwilliams <codeGeek.03.14@gmail.com>
2020-08-25 19:19:03 -0700

    Final Draft

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-const myProblem = ( ) => {
-    var str = "x<sup>2</sup>";
-    var myA = document.getElementById("varA").value;
-    var myB = document.getElementById("varB").value;
-    var a = parseFloat(myA);
-    var b = parseFloat(myB);
-
-    if (a+b === 5 && a*b === 6) {
-        return ("Good Job!");
-    } else  
-        return ("Please try again.");
-}
-const myCalc = ( ) => {
-document.getElementById("demo").innerHTML = myProblem( );
+const myProblem = () => {
+    var a = document.getElementById("varA").value;
+    var b = document.getElementById("varB").value;
+    if (a == "2" && b == "3") {
+        return "Good Job!";
+    } else
+        return "Try Again!";
+};
+const myCalc = () => {
+    document.getElementById("demo1").innerHTML = myProblem();
 }
 
-// TEXT-BOX: SOLUTIONS TO EXAMPLE 
-const getImage = ( ) =>  {
-const solution = ("(x + 2)(x + 3)" + " or " + 
-    "(x + 3)(x + 2)");
-    document.getElementById("solutionPic").value;
-        return solution;
-}
-const revealAnswer = ( ) => {
-    document.getElementById("demo2").innerHTML = getImage( );
+const myProblem2 = () => {
+    return "(x + 2)(x + 3) or (x + 3)(x + 2)";
+};
+const myCalc2 = () => {
+    document.getElementById("demo2").innerHTML = myProblem2();
 }
 
+const myProblem3 = () => {
+    var a2 = document.getElementById("varA2").value;
+    var b2 = document.getElementById("varB2").value;
+    if (a2 == "4" && b2 == "5") {
+        return "Good Job!";
+    } else
+        return "Try Again!";
+};
+const myCalc3 = () => {
+    document.getElementById("demo3").innerHTML = myProblem3();
+}
 
-// for image click to reveal solution png
-// function showImage (src, width, height, alt);
-// var blackhelmet = document.createElement("player");
-//     img.src = .win_the_day_player.png,
-//     img.width = auto;
-//     img.height = auto;
-//     img.alt = alt;
-//     document.body.appendChild (img.png);
-// }
-//     <!-- /* // This next line will just add it to the <body> tag -->
-//     <!-- // but you can adapt to make it append to the element you want. */ -->
+const myProblem4 = () => {
+    return "(x + 4)(x + 5) or (x + 5)(x + 4)";
+};
+const myCalc4 = () => {
+    document.getElementById("demo4").innerHTML = myProblem4();
+}
+
+const myProblem5 = () => {
+    var a3 = document.getElementById("varA3").value;
+    var b3 = document.getElementById("varB3").value;
+    if (a3 == "3" && b3 == "8") {
+        return "Good Job!";
+    } else
+        return "Try Again!";
+};
+const myCalc5 = () => {
+    document.getElementById("demo5").innerHTML = myProblem5();
+}
+
+const myProblem6 = () => {
+    return "(x + 3)(x + 8) or (x + 8)(x + 3)";
+};
+const myCalc6 = () => {
+    document.getElementById("demo6").innerHTML = myProblem6();
+}
+
